b80aa835415cc1bdd5fa249b5097cd4ba8cffefb
Tuan30 <anhtuan28023008@gmail.com>
2022-05-10 20:50:14 +0700

    Xong bài 16

--- app/src/main/java/nhom8/android_coding/sneaker_app/utils/Utils.java
+++ app/src/main/java/nhom8/android_coding/sneaker_app/utils/Utils.java
@@ -2,5 +2,5 @@ package nhom8.android_coding.sneaker_app.utils;
 
 public class Utils {
 
-    public static final String BASE_URL = "http://192.168.1.18/Sneaker/";
+    public static final String BASE_URL = "http://192.168.152.31/Sneaker/";
 }
